1de9c6ac5acf470e1a9335898cd827786c94f243
Maciej Waszkiewicz <macieq091@gmail.com>
2023-08-18 21:10:25 +0200

    Adding header and trying to choose a background

---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import classes from '../layout/Header.module.css'
+
+const Header = props => {
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>Crossfit notebook</h1>
+			</header>
+			
+		</Fragment>
+	)
+}
+
+export default Header;
